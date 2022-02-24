fd8951c21104d4fa87515b6debdb40ca7fa1dce2
aniamrstc <ania.mrstc@eduge.ch>
2022-02-24 09:13:27 +0100

    composer

---
+++ view/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+<div class="container-fluid main-panel" style="margin-top:10%; margin-bottom:10%;">
+        <div class=" row">
+            <div class="col d-flex justify-content-center align-items-center">
+                <div class="login-panel">
+                    <div class="login-user-avatar"><img src="img/user-icon.jpg" alt="user-icon"></div>
+                    <div class="login-form">
+                        <form action="#" method="POST">
+                            <div class="form-group mb-3">
+                                <div class="input-group"><input class="form-control" type="email" id="email" name="email" placeholder="Email" required></div>
+                            </div>
+                            <div class="form-group mb-3">
+                                <div class="input-group"><input class="form-control" type="password" name="password" placeholder="Mot de passe" required></div>
+                            </div>
+                            <div class="form-group mb-3"><input class="btn btn-primary d-block w-100" type="submit" name="btnSubmit" value="Se connecter"></div>
+                        </form>
+                    </div>
+                    <br>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
