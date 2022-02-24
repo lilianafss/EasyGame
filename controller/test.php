f4ca5ddf52c2cec7071a07091d654fc9d09952b2
FiestyFalconer <60130986+FiestyFalconer@users.noreply.github.com>
2022-02-24 08:55:20 +0100

    methode MVC

---
+++ controller/test.php
@@ -0,0 +1,43 @@
+<?php
+$password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING);
+$realpassword = filter_input(INPUT_POST,'realpassword',FILTER_SANITIZE_STRING);
+$hash = "";
+$seePassword = "";
+
+if($password != "" && $realpassword != ""){
+    $hash = password_hash($password, PASSWORD_BCRYPT);
+    $seePassword = password_verify($realpassword, $hash);
+}
+
+if($hash != ""){
+    var_dump($hash);
+    var_dump($realpassword);
+    if($seePassword){
+        echo "<h1>Le mot de passe est valide!</h1>";
+    }else{
+        echo "<h1>Le mot de passe est invalide.</h1>";
+    }
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="POST" action="test.php">
+        <input type="text" name="password">
+        <br>
+        <label>---------------------------------</label>
+        <br>
+        <input type="text" name="realpassword">
+        <br>
+        <input type="submit" value="send">
+    </form>
+</body>
+</html>
