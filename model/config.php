d42edc4fd8433453c701faceabfbb77cbafc1edb
FiestyFalconer <60130986+FiestyFalconer@users.noreply.github.com>
2022-02-24 09:27:34 +0100

    connexion base de données

---
+++ model/config.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Auteur      : De Castilho E Sousa Rodrigo
+Description : Constantes pour la connexion à la base de données
+Date        : 02/2022
+Version     : 1.0.0.0
+*/
+
+//Constantes pour la connexion à la base de données
+define('DB_HOST', 'LocalHost');
+define('DB_NAME', 'easygame');
+define('DB_USER', 'userEasyGame');
+define('DB_PASSWORD', 'Super2022');
