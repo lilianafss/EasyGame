8c8f90c8092f6c96673c370c075b01a2d1bd7eea
lilianafss <liliana.sntss@eduge.ch>
2022-03-02 15:03:33 +0100

    controleur accueil

---
+++ src/Controller/accueilController.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace EasyGame\Controller;
+    use EasyGame\model\FonctionsBd;
+
+    class accueilController
+    {
+        public function accueil()
+        {
+            require "../view/accueil.php";
+        }
+    }
+
+?>
