bc40b62121446a7d2845be7bef0777adbf067da8
aniamrstc <ania.mrstc@eduge.ch>
2022-03-03 09:21:43 +0100

    correction des routes

--- router/web.php
+++ router/web.php
@@ -1,13 +1,14 @@
 <?php
-use EasyGame\Controller\connexionController;
-use EasyGame\Controller\registerController;
-
 use Pecee\SimpleRouter\SimpleRouter;
 use PHPMailer\PHPMailer\src\PHPMailer;
 use PHPMailer\PHPMailer\src\SMTP;
 use PHPMailer\PHPMailer\src\Exception;
 
+use EasyGame\Controller\connexionController;
+use EasyGame\Controller\registerController;
+use EasyGame\Controller\accueilController;
 
-SimpleRouter::form('/', [connexionController::class, 'connexion']);
+
+SimpleRouter::form('/', [accueilController::class, 'accueil']);
 SimpleRouter::form('/connexion', [connexionController::class, 'connexion']);
 SimpleRouter::form('/nouveau', [registerController::class, 'nouveauCompte']);
