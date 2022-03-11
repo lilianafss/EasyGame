8df6af0ef4b8308810f83c9f96b22ee9af4a9ba4
LethalDarkLight <darkmihawk1290@gmail.com>
2022-03-11 11:10:25 +0100

    règlage de fichier manquant

--- router/web.php
+++ router/web.php
@@ -4,9 +4,9 @@ use PHPMailer\PHPMailer\src\PHPMailer;
 use PHPMailer\PHPMailer\src\SMTP;
 use PHPMailer\PHPMailer\src\Exception;
 
-use EasyGame\Controller\connexionController;
-use EasyGame\Controller\registerController;
-use EasyGame\Controller\accueilController;
+use EasyGame\Controller\ConnexionController;
+use EasyGame\Controller\RegisterController;
+use EasyGame\Controller\AccueilController;
 
 SimpleRouter::form('/', [AccueilController::class, 'accueil']);
 SimpleRouter::form('/connexion', [ConnexionController::class, 'connexion']);
