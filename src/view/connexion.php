93f0bfe9f04ff65af2ac0ba499dd64a44d62a900
Rodrigo Sousa <rodrigo.dcstl@eduge.ch>
2022-03-16 13:44:14 +0100

    css page de connexion

--- src/view/connexion.php
+++ src/view/connexion.php
@@ -1,34 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/assets/css/connexion.css">
 </head>
+
 <body>
-        <div class=" row">
-            <div class="col d-flex justify-content-center align-items-center">
-                        <form method="POST">
-                            <div class="form-group mb-3">
-                                <div><input type="email" id="email" name="email" placeholder="Email"></div>
-                            </div>
-                            <div class="form-group mb-3">
-                                <div><input type="password" name="password" placeholder="Mot de passe"></div>
-                            </div>
-                            <div><input  type="submit" name="btnSubmit" value="Se connecter"></div>
-                        </form>
+    <main>
+        <div class="row d-flex justify-content-center" id="box">
+            <div class="row text-center" id="title">
+                <h3>Connexion</h3>
+            </div>
+            <div class="col justify-content-center align-items-center">
+                <form method="POST">
+                    <div class="form-group mb-3">
+                        <div><input type="email" id="email" name="email" placeholder="Email" width="100"></div>
                     </div>
-                    
-                    <br>
-                    <div>
-                        <p>
-                            <?=$erreur?>
+                    <div class="form-group mb-3">
+                        <div><input type="password" name="password" placeholder="Mot de passe"></div>
+                    </div>
+                    <div class="form-group mb-3">
+                        <p class="text-danger">
+                            <?= $erreur ?>
                         </p>
                     </div>
-                </div>
+                    <div class="form-group mb-3">
+                        <div><input type="submit" name="btnSubmit" value="Se connecter"></div>
+                    </div>
+                </form>
+
+                <a href="http://easygame">Mot de passe oublié?</a>
             </div>
-        </div>
-    </div>
+            
+            <div>
+            <hr id="line">
+            </div>
+
+            <div class="col justify-content-center align-items-center" id="linkIncrit">
+                <a href="http://easygame/nouveau">S'incrire</a>
+            </div>
+    </main>
+    <?php require_once 'footer.html' ?>
 </body>
+
 </html>
--- src/view/register.php
+++ src/view/register.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="/assets/css/register.css">
 </head>
 <body class="d-flex flex-column h-100">
