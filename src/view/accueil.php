d884ebaa16c8c5588f783231974e7a91119cbf72
LethalDarkLight <darkmihawk1290@gmail.com>
2022-03-16 16:23:25 +0100

    Changement de la View page Accueil

--- src/view/accueil.php
+++ src/view/accueil.php
@@ -1,112 +1,133 @@
 <?php
-@ini_set('display_errors', 'on');
-session_start();
-//require_once "../model/FonctionsBD.php"; 
-// use function EasyGame\model\getGames;
-use EasyGame\model\FonctionsBD;
-use EasyGame\model\BaseDonnee;
-$listeAge=FonctionsBD::getPegi();
-$listePlateforme=FonctionsBD::getPlatform();
-$listeGenre=FonctionsBD::getGenre();
-var_dump($_SESSION);
-var_dump(FonctionsBD::getComments(1));
+    @ini_set('display_errors', 'on');
+    session_start();
+//    require_once "../model/FonctionsBD.php";
+//     use function EasyGame\model\getGames;
+    use EasyGame\model\FonctionsBD;
+    use EasyGame\model\BaseDonnee;
+    $listeAge=FonctionsBD::getPegi();
+    $listePlateforme=FonctionsBD::getPlatform();
+    $listeGenre=FonctionsBD::getGenre();
+//    var_dump($_SESSION);
+//    var_dump(FonctionsBD::getComments(1));
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/logo/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="assets/css/style.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="/assets/css/footer.css">
 </head>
-<body>
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
-        <div class="container-fluid">
-            <div class="collapse navbar-collapse" id="mynavbar">
-                <form class="d-flex">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <select name="age" id="age" class="form-control border-0 px-2 py-1 mx-2 rounded shadow" style="width: 100px;">
-                                <option value="" disabled selected>Âge</option>
+
+<body class="d-flex flex-column h-100">
+    <header>
+        <nav class="navbar justify-content-center">
+            <a href="/">
+                <img id="logo" alt="logo" src="assets/logo/logo.png">
+            </a>
+            <form method="post">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <select name="age" id="age" class="border-0 px-2 py-1 m-2 rounded shadow" style="width: 100px;">
+                            <option value="" disabled selected>Age</option>
                             <?php  foreach($listeAge as $age) {?>
                                 <option value="<?php $age["pegi"]?>" onclick="afficherFiltre()"><?php echo $age["pegi"]?></option>
-                                <?php } ?>
-                            </select>
-                            
-                        </li>
-                        <li class="nav-item">
-                            <select name="plateforme" id="plateforme" class="form-control border-0 px-2 py-1 mx-2 rounded shadow" style="width: 175px;">
-                                <option value="" disabled selected>Toutes les plateformes</option>
-                                <?php  foreach($listePlateforme as $plateforme) {?>
+                            <?php } ?>
+                        </select>
+                    </li>
+
+                    <li class="nav-item">
+                        <select name="plateforme" id="plateforme" class="border-0 px-2 py-1 m-2 rounded shadow" style="width: 175px;">
+                            <option value="" disabled selected>Plateformes</option>
+                            <?php  foreach($listePlateforme as $plateforme) {?>
                                 <option value="<?php $plateforme["plateforme"]?>" onclick="afficherFiltre()"><?php echo $plateforme["plateforme"]?></option>
-                                <?php } ?>
-                                
-                            </select>
-                        </li>
-                        <li class="nav-item">
-                            <select name="genre" id="genre" class="form-control border-0 px-2 py-1 mx-2 rounded shadow" style="width: 150px;">
-                                <option value="" disabled selected>Tous les genres</option>
-                                <?php  foreach($listeGenre as $genre) {?>
-                                
+                            <?php } ?>
+
+                        </select>
+                    </li>
+
+                    <li class="nav-item">
+                        <select name="genre" id="genre" class="border-0 px-2 py-1 m-2 rounded shadow" style="width: 150px;">
+                            <option value="" disabled selected>Genres</option>
+                            <?php  foreach($listeGenre as $genre) {?>
+
                                 <option value="<?php $genre["genre"]?>" onclick="afficherFiltre()"><?php echo $genre["genre"]?></option>
-                                <?php } ?>
-                            </select>
-                        </li>
-                        <li class="nav-item">
-                            <input class="form-control me-2" type="Search" placeholder="Recherche">
-                            <button class="btn btn-primary my-2 my-sm-0" type="submit">Rechercher</button>
-                        </li>
-                    </ul>
+                            <?php } ?>
+                        </select>
+                    </li>
 
-                </form>
-            </div>
-        </div>
-    </nav>
+                    <li class="nav-item">
+                        <input class="border-0 px-2 py-1 m-2 rounded shadow" type="Search" placeholder="Recherche">
+                        <button class="btn btn-primary" type="submit">Rechercher</button>
+                    </li>
+                </ul>
+            </form>
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Connexion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Panier</a>
+                </li>
+            </ul>
 
-    <div class="container-fluid container_jeux">
+        </nav>
+    </header>
+
+    <main class="flex">
+        <div id="divMain">
             <?php
-            $listeJeux=FonctionsBD::getGames();
-            $listeFiltre=FonctionsBD::getGameByFilters("","","");
-            var_dump($listeFiltre);
-                if($listeFiltre!=""){
-                    foreach($listeFiltre as $filtre){
-            ?>
-
-                    <div class="card flex-row flex-wrap m-4">
-                        <?php echo '<img class="card-img" src="data:image/jpeg;base64,'.base64_encode( $filtre['image'] ).'"/>'; ?>
-                        <div class="card-block p-2">
-                            <h4 class="card-title"><?php echo $filtre['nom']?></h4>
-                            <p class="card-text"><?php echo $filtre['description']?></p>
-                            <p class="card-text card_prix"><?php echo $filtre['prix']?></p>
-                            <a href="#" class="btn btn-primary mx-auto d-block">Ajouter au panier</a>
+                $listeJeux=FonctionsBD::getGames();
+                $listeFiltre=FonctionsBD::getGameByFilters("","","");
+                //var_dump($listeFiltre);
+                if($listeFiltre!="")
+                {
+                    foreach($listeFiltre as $filtre)
+                    {
+                        ?>
+                        <div class="card">
+                            <?php echo '<img class="card-img" src="data:image/jpeg;base64,'.base64_encode( $filtre['image'] ).'"/>'; ?>
+                            <div class="card-block">
+                                <h4 class="card-title"><?php echo $filtre['nom']?></h4>
+                                <section class="card-text">
+                                    <p><?php echo $jeux['description']?></p>
+                                </section>
+                                <p class="card_prix"><?php echo $filtre['prix']?></p>
+                                <a href="#" class="btn btn-primary card-btn">Ajouter au panier</a>
+                            </div>
                         </div>
-                    </div>
-            <?php       
+                        <?php
                     }
-                }else{
-                    foreach($listeJeux as $jeux){
-            ?>
-                   <div class="card flex-row flex-wrap m-4">
-                        <?php echo '<img class="card-img" src="data:image/jpeg;base64,'.base64_encode( $jeux['image'] ).'"/>'; ?>
-                        <div class="card-block p-2">
-                            <h4 class="card-title"><?php echo $jeux['nom']?></h4>
-                            <p class="card-text"><?php echo $jeux['description']?></p>
-                            <p class="card-text card_prix"><?php echo $jeux['prix']?></p>
-                            <a href="#" class="btn btn-primary mx-auto d-block">Ajouter au panier</a>
+                }
+                else
+                {
+                    foreach($listeJeux as $jeux)
+                    {
+                        ?>
+                        <div class="card m-4">
+                            <?php echo '<img class="card-img" src="data:image/jpeg;base64,'.base64_encode( $jeux['image'] ).'"/>'; ?>
+                            <div class="card-block">
+                                <h4 class="card-title"><?php echo $jeux['nom']?></h4>
+                                <section class="card-text">
+                                    <p><?php echo $jeux['description']?></p>
+                                </section>
+                                <p class="card_prix"><?php echo $jeux['prix']?>€</p>
+                                <a href="#" class="btn btn-primary card-btn">Ajouter au panier</a>
+                            </div>
                         </div>
-                    </div>
-            <?php
+                        <?php
                     }
                 }
             ?>
-        </div>   
-
+        </div>
+    </main>
+    <?php require_once"footer.html"; ?>
 </body>
-<footer>
-
-<?php include "footer.html" ?>
-</footer>
 </html>
--- src/view/register.php
+++ src/view/register.php
@@ -5,36 +5,45 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="stylesheet" href="/assets/css/register.css">
     <link rel="icon" type="image/png" sizes="16x16" href="assets/logo/logo.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/register.css">
+    <link rel="stylesheet" href="/assets/css/footer.css">
 </head>
+
 <body class="d-flex flex-column h-100">
     <header>
             <nav class="navbar justify-content-center">
+
                 <a href="/">
                     <img id="logo" alt="logo" src="assets/logo/logo.png">
                 </a>
+
                 <ul class="nav">
                     <li class="nav-item">
                         <a class="nav-link" href="/">Accueil</a>
                     </li>
+
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link</a>
                     </li>
+
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link</a>
                     </li>
+
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link</a>
                     </li>
+
                     <li class="nav-item">
                         <a class="nav-link" href="#">Link</a>
                     </li>
                 </ul>
             </nav>
     </header>
+
     <main class="flex-shrink-0">
         <h1>Insciption</h1>
         <div id="divMain">
@@ -69,6 +78,7 @@
                     <label for="password2" class="label-input"> Confirmer mot de passe </label>
                     <input id="password2" class="label-input" type="password" name="password2" minlength="4" value="">
                 </div>
+
                 <label for="showPassWord" id="containerShowPassWord">
                     <input type="checkbox" name="showPassWord" class="col-2" id="showPassWord" onclick="HidePassword()">
                     <span class="col-10">Afficher le mot de passe</span>
@@ -80,37 +90,10 @@
                 </div>
 
                 <span id="errorMessage"><?=$message?></span>
-
             </form>
         </div>
     </main>
-    <footer class="mt-auto pt-3 text-white">
-        <div id="divFooter">
-            <section class="sectionFooter">
-                <i class="fa fa-2x fa-paypal"></i>
-                <i class="fa fa-2x fa-cc-visa"></i>
-                <i class="fa fa-2x fa-cc-mastercard"></i>
-            </section>
-
-            <p>© 2022 Copyright</p>
-
-            <section class="sectionFooter">
-                <a class="btn btn-outline-light btn-floating m-1" href="https://fr-fr.facebook.com" role="button">
-                    <i class="fa fa-facebook"></i>
-                </a>
-
-                <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com " role="button">
-                    <i class="fa fa-twitter"></i>
-                </a>
-
-                <a class="btn btn-outline-light btn-floating m-1" href="https://www.instagram.com " role="button">
-                    <i class="fa fa-instagram"></i>
-                </a>
-
-                <p>site.easygame@gmail.com</p>
-            </section>
-        </div>
-    </footer>
+    <?php require_once"footer.html"; ?>
     <script  src="assets/js/hidePassword.js"></script>
 </body>
 </html>
