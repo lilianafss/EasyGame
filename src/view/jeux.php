517b804b9d011c0f9f831da9bf4390c929e8f55d
Liliana Santos <liliana.sntss@eduge.ch>
2022-03-21 08:01:25 +0100

    page jeux

--- src/view/jeux.php
+++ src/view/jeux.php
@@ -5,11 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
 </head>
 <body> 
-    <?php
-    var_dump($_GET['idJeux']);
-    ?>
     <h1><?=$infoJeux['nom']?></h1>
     <?php echo '<img class="card-img" src="data:image/jpeg;base64,'.base64_encode( $infoJeux['image'] ).'"/>'; ?>
     <h3>A propos du jeu</h3>
@@ -21,5 +20,7 @@
     <p>Pseudo</p>
     <p>Note</p>
     <p>Commentaire</p>
+
+    <?php require_once "footer.php"; ?>
 </body>
 </html>
