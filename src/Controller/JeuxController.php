4b3e5b0b1833bf42f5cbf4619434bb437767f280
Liliana Santos <liliana.sntss@eduge.ch>
2022-04-07 15:08:59 +0200

    modification de la vue de la page jeux

--- src/Controller/JeuxController.php
+++ src/Controller/JeuxController.php
@@ -16,8 +16,7 @@ class JeuxController
 
         if ($idJeux != "") {
 
-            $content = "";
-            $formulaire = "";
+            
             $stringUser = "";
             $stringNote = "";
             $stringCommentaire = "";
@@ -29,23 +28,21 @@ class JeuxController
             $tableauxCommentaire = FonctionsBD::getComments($idJeux);
             $tableauxNotes = FonctionsBD::getNotes($idJeux);
 
-            $submit = filter_input(INPUT_POST, 'envoyer', FILTER_SANITIZE_SPECIAL_CHARS);
+            $submit = filter_input(INPUT_POST, 'submit', FILTER_SANITIZE_SPECIAL_CHARS);
 
-
+        
             //Si le button envoyer est egal a "Ajouter commentaire"
-            if ($submit = "Ajouter commentaire") {
+            if ($submit = "Ajouter") {
 
                 //Récupération des notes et commentaires
                 $note = filter_input(INPUT_POST, 'note', FILTER_SANITIZE_NUMBER_INT);
                 $commentaire = filter_input(INPUT_POST, 'commentaire', FILTER_SANITIZE_SPECIAL_CHARS);
-
                 //Si les input commentaire et note n'est pas egal a vide
-                if ($commentaire != "" && $note != "") {
+                if ($commentaire != "" && $note != "" &&  $userUtilisateur != "") {
                     //ajouter les notes et commentaires a la base de donnees
                     FonctionsBD::addCommentToGame($commentaire, $idJeux, $userUtilisateur);
                     FonctionsBD::addNoteToGame($note, $idJeux, $userUtilisateur);
-                }
-                if ($commentaire != "") {
+                } elseif ($commentaire != "" && $userUtilisateur != "") {
                     FonctionsBD::addCommentToGame($commentaire, $idJeux, $userUtilisateur);
                 }
             }
@@ -66,106 +63,91 @@ class JeuxController
             }
 
             if ($_SERVER['REQUEST_METHOD'] == "POST") {
-                if($_POST['panier']){
-                    
-                    $panier=FonctionsBD::addGameToPanier($userUtilisateur,$idJeux);
-                    $envoiePanier="Dans le panier";
-                      
+                if ($_POST['panier']) {
+
+                    $panier = FonctionsBD::addGameToPanier($userUtilisateur, $idJeux);
+                    $envoiePanier = "Dans le panier";
                 }
             }
+            // }
+
+            //Affichage des l'informations du jeux
+            // $content =
+            //     '<div class="description">
+            //         <h1>' . $infoJeux['nom'] . '</h1>
+            //         <img src="data:image/jpeg;base64,' . base64_encode($infoJeux['image']) . '"/>
+            //         <h3>A propos du jeu</h3>
+            //         <p>' . $infoJeux['description'] . '</p>
+            //     </div>
+  
+            //     <input type="submit" name="panier" id="panier" value="Ajouter au panier"><br>
+             
+            //      <br>';
+            //     if($stringCommentaire!=""){
+            //         $content.='
+            //         <h3>Notes et Comentaires</h3>
+            //         <div class="review-list">
+            //             <ul>
+            //                 <li>
+            //                     <div class="right">
+            //                         <h4>' . $stringUser . '
+            //                             <span class="gig-rating text-body-2">
+            //                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" width="15" height="15">
+            //                                     <path
+            //                                         fill="currentColor"
+            //                                         d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"
+            //                                     ></path>
+            //                                 </svg>
+            //                                 ' . $stringNote . '
+            //                             </span>
+            //                         </h4>
+            //                         <span class="publish py-3 d-inline-block w-100">' . $stringDate . '</span>
+            //                         <div class="review-description">
+            //                             <p>
+            //                             ' . $stringCommentaire . '
+            //                             </p><br>
+            //                         </div>
+                                    
+            //                     </div>
+            //                 </li>
+            //             </ul>
+            //         </div>
+            //         ';
                 // }
 
+            // if ($userUtilisateur != "") {
+            //     $formulaire = '<div class="bg-white rounded shadow-sm p-4 mb-5 rating-review-select-page">
+            //         <h5 class="mb-4">Laisse votre avis</h5>
+            //         <label>Note</p>
+            //         <input type="number" min="1" max="5" name="note" id="note"><br>
+            //         <div class="form-group">
+            //             <label>Votre commentaire</label>
+            //             <textarea class="form-control" name="commentaire" id="commentaire" required></textarea>
+            //         </div>
+            //         <div class="form-group">
+            //             <input type="submit" value="Ajouter commentaire" name="envoyer">
+            //         </div>
 
-                //Affichage des l'informations du jeux
-                $content .=    
-                '<div class="description">
-                    <h1>' . $infoJeux['nom'] . '</h1>
-                    <img src="data:image/jpeg;base64,' . base64_encode($infoJeux['image']) . '"/>
-                    <h3>A propos du jeu</h3>
-                    <p>' . $infoJeux['description'] . '</p>
-                </div>
-                <form method="POST">
-                <input type="submit" name="panier" id="panier" value="Ajouter au panier"><br>
-                </form>
-                 <br>
-                <h3>Notes et Comentaires</h3>
-                <div class="review-list">
-                    <ul>
-                        <li>
-                            <div class="right">
-                                <h4>'.$stringUser.'
-                                    <span class="gig-rating text-body-2">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" width="15" height="15">
-                                            <path
-                                                fill="currentColor"
-                                                d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"
-                                            ></path>
-                                        </svg>
-                                        '.$stringNote.'
-                                    </span>
-                                </h4>
-                                <span class="publish py-3 d-inline-block w-100">'.$stringDate.'</span>
-                                <div class="review-description">
-                                    <p>
-                                    '.$stringCommentaire.'
-                                    </p>
-                                </div>
-                                
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-                ';
-
-                if ($userUtilisateur != "") {
-                    $formulaire = '                
-                    <div class="bg-white rounded shadow-sm p-4 mb-5 rating-review-select-page">
-                        <h5 class="mb-4">Laisse votre avis</h5>
-                        <p class="mb-2">Note</p>
-                        <input type="number" min="1" max="5" name="note" id="note"><br>
-                        <form>
-                            <div class="form-group">
-                                <label>Votre commentaire</label>
-                                <textarea class="form-control" name="commentaire" id="commentaire" required></textarea>
-                            </div>
-                            <div class="form-group">
-                                <input type="submit" value="Ajouter commentaire" name="envoyer">
-                            </div>
-                        </form>
-                    </div>
-                
-                ';
-          
-
-            if($userUtilisateur!=""){
-                $formulaire ='<div class="evaluation">
-                <label for="note">Note :</label>
-                <input type="number" min="1" max="5" name="note" id="note"><br>
-                <br>
-                <label for="commentaire" >Commentaire: </label>
-                <textarea name="commentaire" id="commentaire" cols="50" rows="5" required></textarea><br>
-                <br>
-                <input type="submit" value="Ajouter commentaire" name="envoyer">
-                </div>';
+            //     </div>';  
 
 
-                foreach($tableauxNotes as $note){
-                    if($userUtilisateur == $note['idUser']){
-                        $formulaire = '<div class="evaluation">
-                        <label for="commentaire" >Commentaire: </label>
-                        <textarea name="commentaire" id="commentaire" cols="50" rows="5" required></textarea>
-                
-                        <input type="submit" value="Ajouter commentaire" name="envoyer">
-                        </div>';
-                        }
-                    }
+            //     foreach ($tableauxNotes as $note) {
+            //         if ($userUtilisateur == $note['idUser']) {
+            //             $formulaire = '<div class="evaluation">
+            //                 <label for="commentaire" >Commentaire: </label>
+            //                 <textarea name="commentaire" id="commentaire" cols="50" rows="5" required></textarea>
+            //                <input type="submit" value="Ajouter" name="submit">
+            //             </div>'; 
+            //         }
+            //     }
 
-                    $content .= $formulaire;
-                }
-            } else {
-                header("Location: http://easygame.ch/");
-            }
-            require '../src/view/jeux.php';
+            //     $content .= $formulaire;
+            // }
+        } else {
+            header("Location: http://easygame.ch/");
+            
         }
+
+        require '../src/view/jeux.php';
     }
 }
--- src/view/jeux.php
+++ src/view/jeux.php
@@ -1,3 +1,7 @@
+<?php
+$content = "";
+$formulaire = "";
+?>
 <!DOCTYPE html>
 <html lang="en" class="h-100">
 <head>
@@ -12,7 +16,62 @@
 <body  class="h-100"> 
     <?php require_once "header.php"; ?>
  
-    <form method="POST"> 
+    <main>
+        <form method="POST"> 
+            <div class="description">
+                <h1><?=$infoJeux['nom']?></h1>
+                <?php echo '<img class="card-img" src="data:image/jpeg;base64,'.base64_encode( $infoJeux['image'] ).'"/>'; ?>
+                <h3>A propos du jeu</h3>
+                <p><?=$infoJeux['description']?></p>
+            </div>
+
+            <input type="submit" name="panier" id="panier" value="Ajouter au panier"><br>
+        
+            <h3>Notes et Comentaires</h3>
+            <div class="review-list">
+                <ul>
+                    <li>
+                        <div class="right">
+                            <h4><?=$stringUser?>
+                                <span class="gig-rating text-body-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1792 1792" width="15" height="15">
+                                        <path
+                                            fill="currentColor"
+                                            d="M1728 647q0 22-26 48l-363 354 86 500q1 7 1 20 0 21-10.5 35.5t-30.5 14.5q-19 0-40-12l-449-236-449 236q-22 12-40 12-21 0-31.5-14.5t-10.5-35.5q0-6 2-20l86-500-364-354q-25-27-25-48 0-37 56-46l502-73 225-455q19-41 49-41t49 41l225 455 502 73q56 9 56 46z"
+                                        ></path>
+                                    </svg>
+                                    <?=$stringNote ?>
+                                </span>
+                            </h4>
+                            <span class="publish py-3 d-inline-block w-100"><?=$stringDate?></span>
+                            <div class="review-description">
+                                <p>
+                                <?=$stringCommentaire?>
+                                </p><br>
+                            </div>
+                            
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="bg-white rounded shadow-sm p-4 mb-5 rating-review-select-page">
+                <h5 class="mb-4">Laisse votre avis</h5>
+                <label>Note</p>
+                <input type="number" min="1" max="5" name="note" id="note"><br>
+                <div class="form-group">
+                    <label>Votre commentaire</label>
+                    <textarea class="form-control" name="commentaire" id="commentaire" required></textarea>
+                </div>
+                <div class="form-group">
+                    <input type="submit" value="Ajouter commentaire" name="envoyer">
+                </div>
+
+            </div>
+        </form>
+    </main>
+    
+
         <?=$content?>
         <!-- <fieldset class="rating"> 
             <input name="rating" type="radio" id="rating5" value="5" on="change:rating.submit">
@@ -31,8 +90,7 @@
             <label for="rating1" title="1 stars">☆</label>
         </fieldset> 
         -->
-        
-    </form>
+ 
 
 
     <?php require_once "footer.php"; ?>
