bb8ed19af89d845f4886b39ed16d961b65d7fe8a
Flavio Soares Rodrigues <darkmihawk1290@gmail.com>
2022-04-27 13:35:11 +0200

    gitignore test

--- src/php/config.php
+++ src/php/config.php
@@ -15,3 +15,6 @@ const DB_PASSWORD = 'uS0_q2BLFj4ds';
 // Constantes pour la connexion au mail avec phpmailer
 const MAIL_USERNAME ='site.easygame@gmail.com';
 const MAIL_PASSWORD ='aHi!Lc5_n8L0-ya';
+
+//test
+$test = "";
