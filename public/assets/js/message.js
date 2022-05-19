d5cf73aba725b76a01c71ca95af688f728f68480
Flavio Soares Rodrigues <darkmihawk1290@gmail.com>
2022-05-19 13:33:57 +0200

    balise style ajouter dans jeux.php

--- public/assets/js/message.js
+++ public/assets/js/message.js
@@ -3,16 +3,10 @@ function Message (statment)
     if (statment === "fail")
     {
         document.getElementById('message').style.display = 'block';
-        document.getElementById('message').style.background = 'rgba(200, 30, 50, 0.5)';
-        document.getElementById('message').style.color = 'darkred';
-        document.getElementById('message').style.border = '2px solid darkred';
         document.getElementById('email').focus();
     }
     else if (statment === "sucess")
     {
         document.getElementById('message').style.display = 'block';
-        document.getElementById('message').style.background = 'rgba(30, 190, 50, 0.5)';
-        document.getElementById('message').style.color = 'darkgreen';
-        document.getElementById('message').style.border = '2px solid darkgreen';
     }
 }
