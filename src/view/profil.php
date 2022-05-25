05cf6fb54254187925e55be5b706a22561af0d3e
ania marostica <ania.mrstc@eduge.ch>
2022-05-25 08:42:43 +0200

    debut css historique

--- src/view/profil.php
+++ src/view/profil.php
@@ -58,8 +58,9 @@ $tableauxHistorique = HistoriqueModel::getHistory($_SESSION['idUser'])
         <div class="alert alert-danger alert-dismissible d-flex align-items-center fade show">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi-exclamation-octagon-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
                 <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
-                <?= $errorMessage ?>
-                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+            </svg>
+            <?= $errorMessage ?>
+            <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
         </div>
     <?php
     } ?>
@@ -121,7 +122,7 @@ $tableauxHistorique = HistoriqueModel::getHistory($_SESSION['idUser'])
 
 
 
-           
+
             <div id="historiqueAchat" class="tabcontent">
                 <div id="container-historique" class="container">
                     <table id="cart" class="table table-hover table-condensed">
