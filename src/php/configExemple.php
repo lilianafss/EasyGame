02da8487476c54740addd5e8d0756bced8b06561
Liliana Santos <liliana.sntss@eduge.ch>
2022-06-02 14:07:49 +0200

    Create configExemple.php

---
+++ src/php/configExemple.php
@@ -0,0 +1,15 @@
+<?php
+
+// Constantes pour la connexion à la base de données
+const DB_HOST = ''; // ip 
+const DB_NAME = ''; // nom base de données
+const DB_USER = ''; // utilisateur base de données
+const DB_PASSWORD = ''; // mot de passe base de donnees
+
+// Constantes pour la connexion au mail avec phpmailer
+const MAIL_USERNAME =''; //Completer avec l'email de "l'entreprise"
+const MAIL_PASSWORD =''; //Completer avec le mot de passe
+
+//Url de base du site
+const URL_PRINCIPAL=''; //L'url du site. Par exemple:"http://easygame.ch"
+
